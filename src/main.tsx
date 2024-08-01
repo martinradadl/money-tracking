b20cc9634daba2c16f339c707ba57a4784c9eba2
cristiancho29 <cristiancho29@gmail.com>
2024-08-01 09:22:25 -0500

    [fix] Set eslint configuration correctly

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { NavBar } from "./components/nav-bar.tsx";
 import { MainRouter } from "./pages/main-router.tsx";
+import { StrictMode } from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <StrictMode>
     <MainRouter />
-  </React.StrictMode>
+  </StrictMode>
 );
