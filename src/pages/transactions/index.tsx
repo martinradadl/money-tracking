6bacd46c9903bf202fd6569f5d252d867d97ecc8
Martin Rada <104103475+martinradadl@users.noreply.github.com>
2024-08-12 18:49:05 -0500

    Balance added (#9)

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -2,14 +2,39 @@ import React from "react";
 import { Transaction } from "./transaction";
 import { transactions } from "../../data/transactions";
 import AddTransactionModal from "./add-transaction";
+import classNames from "classnames";
 
 export const Transactions: React.FC = () => {
+  const getBalance = () => {
+    let balance = 0;
+    transactions.forEach((elem) => {
+      balance += elem.amount * (elem.type === "income" ? 1 : -1);
+    });
+    return balance;
+  };
+
   return (
-    <div className="h-full flex flex-col px-4">
-      <h1 className="mx-auto py-2 text-[2rem] text-beige">
-        <b>Transactions</b>
+    <div className="h-full flex flex-col px-4 gap-4">
+      <h1 className="mx-auto py-2 text-4xl text-beige font-semibold">
+        Transactions
       </h1>
-      <div className="flex flex-col gap-2">
+      <div className="flex w-full gap-3 pl-2 py-1 text-2xl">
+        <p className="text-beige">My Balance:</p>
+        <p
+          className={classNames(
+            getBalance() >= 0
+              ? "bg-green-pastel text-navy"
+              : "bg-red-pastel text-beige",
+            "rounded px-2 py-0.5"
+          )}
+        >
+          {`${new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+          }).format(getBalance())}`}
+        </p>
+      </div>
+      <div className="flex flex-col gap-3">
         {transactions.map((elem, i) => {
           return <Transaction key={i} transaction={elem} />;
         })}
