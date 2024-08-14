b5e9ce9eb1a83ed1a4d9ebf4c0c93278c96202ad
Martin Rada <martinradadl@gmail.com>
2024-08-14 12:30:41 -0500

    Balance Font semibold and padding at the bottom added

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -9,18 +9,18 @@ export const Transactions: React.FC = () => {
   const [transactionsList] = useRecoilState(transactionsListState);
 
   return (
-    <div className="flex flex-col flex-1 pb-3 px-4 gap-4 overflow-y-auto">
+    <div className="flex flex-col flex-1 pb-14 px-4 gap-4 overflow-y-auto">
       <h1 className="mx-auto py-2 text-4xl text-beige font-semibold">
         Transactions
       </h1>
-      <div className="flex w-full gap-3 pl-2 py-1 text-2xl">
+      <div className="flex w-full gap-3 pl-2 py-1 text-2xl font-semibold">
         <p className="text-beige">My Balance:</p>
         <p
           className={classNames(
             getBalance(transactionsList) >= 0
               ? "bg-green-pastel text-navy"
               : "bg-red-pastel text-beige",
-            "rounded px-2 py-0.5"
+            "rounded px-2 py-0.5 font-semibold"
           )}
         >
           {`${new Intl.NumberFormat("en-US", {
