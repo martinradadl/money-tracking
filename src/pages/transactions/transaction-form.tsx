b4659dba2bff53ee1e35d397eab3c6e1ec532238
Martin Rada <martinradadl@gmail.com>
2024-09-04 17:39:07 -0500

    HandleChangeAmount added for amount input

--- src/pages/transactions/transaction-form.tsx
+++ src/pages/transactions/transaction-form.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRecoilState } from "recoil";
 import { newTransactionState } from "../../data/transactions";
-
+import { Select } from "@headlessui/react";
 
 const categories: string[] = [
   "Salary",
@@ -25,6 +25,28 @@ export const TransactionForm = () => {
     }
   };
 
+  const handleChangeAmount = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let inputValue = event.target.value;
+    const lastValue = inputValue.substring(inputValue.length - 1);
+    // const notAllowedValues = ["+", "-", "e", "E", "."];
+    // inputValue = inputValue.replace(/[\+]/g, "");
+    // pattern="[^eE\x22]+"
+    const allowedValues = new RegExp("[0-9]+");
+    const hasAllowedValues = inputValue
+      .split("")
+      .every((char) => allowedValues.test(char));
+    if (
+      newTransaction &&
+      inputValue.length <= 12 &&
+      (!lastValue || hasAllowedValues)
+    ) {
+      setNewTransaction({
+        ...newTransaction,
+        [event.target.name]: inputValue,
+      });
+    }
+  };
+
   const handleChangeSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
     if (newTransaction) {
       setNewTransaction({
@@ -38,57 +60,58 @@ export const TransactionForm = () => {
     <div className="flex flex-col gap-4 mt-2">
       <label>
         <p className="capitalize text-2xl mb-2">type</p>
-        <select
-            name="type"
-            id="type"
-            value={newTransaction?.type}
-            onChange={handleChangeSelect}
-            className="w-full h-9 border border-navy rounded"
-          >
-            <option value="income">Income</option>
-            <option value="expenses">Expenses</option>
-          </select>
+        <Select
+          name="type"
+          id="type"
+          value={newTransaction?.type}
+          onChange={handleChangeSelect}
+          className="w-full h-9 border border-navy rounded"
+        >
+          <option value="income">Income</option>
+          <option value="expenses">Expenses</option>
+        </Select>
       </label>
       <label>
         <p className="capitalize text-2xl mb-2">concept</p>
         <input
-            className="w-full h-9 px-2 border border-navy rounded"
-            id="concept"
-            name="concept"
-            value={newTransaction?.concept}
-            onChange={handleChange}
-          />
+          className="w-full h-9 px-2 border border-navy rounded"
+          id="concept"
+          name="concept"
+          value={newTransaction?.concept}
+          onChange={handleChange}
+          maxLength={40}
+        />
       </label>
       <label>
         <p className="capitalize text-2xl mb-2">category</p>
-        <select
-            name="category"
-            id="category"
-            value={newTransaction?.category}
-            onChange={handleChangeSelect}
-            className="w-full h-9 border border-navy rounded"
-          >
-            <option style={{ display: "none" }}></option>
-            {categories.map((elem, i) => {
-              return (
-                <option key={i} value={elem}>
-                  {elem}
-                </option>
-              );
-            })}
-          </select>
+        <Select
+          name="category"
+          id="category"
+          value={newTransaction?.category}
+          onChange={handleChangeSelect}
+          className="w-full h-9 border border-navy rounded"
+        >
+          <option style={{ display: "none" }}></option>
+          {categories.map((elem, i) => {
+            return (
+              <option key={i} value={elem}>
+                {elem}
+              </option>
+            );
+          })}
+        </Select>
       </label>
       <label>
         <p className="capitalize text-2xl mb-2">amount</p>
         <input
-            className="w-full h-9 px-2 border border-navy rounded"
-            type="number"
-            id="amount"
-            name="amount"
-            min="0.1"
-            value={newTransaction?.amount}
-            onChange={handleChange}
-          />
+          className="w-full h-9 px-2 border border-navy rounded"
+          type="text"
+          id="amount"
+          name="amount"
+          // min="0.1"
+          value={newTransaction?.amount}
+          onChange={handleChangeAmount}
+        />
       </label>
     </div>
   );
