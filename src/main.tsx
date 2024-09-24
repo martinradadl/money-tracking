72834eb771d224c296f6b1138bf8651ac8c81f19
Martin Rada <martinradadl@gmail.com>
2024-09-24 15:57:23 -0500

    Toastify added to useTransaction errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { MainRouter } from "./pages/main-router.tsx";
 import { StrictMode } from "react";
 import { RecoilRoot } from "recoil";
+import "./index.css";
+import "toastify-js/src/toastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
