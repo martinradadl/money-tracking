642247f607a66edc6475ec6d08add706b7de9407
Martin Rada <martinradadl@gmail.com>
2024-10-07 12:49:10 -0500

    New tests added, fake data updated

--- src/tests/utils.tsx
+++ src/tests/utils.tsx
@@ -3,11 +3,14 @@ import { LoginI, UserI, userState } from "../data/authentication";
 import { TransactionFormI } from "../data/transactions";
 import React from "react";
 
+export const currencies = [{ name: "fakeCurrency", code: "FAKE" }];
+
 export const newUser: UserI = {
   _id: "fakeId",
   name: "fakeName",
   email: "fakeEmail",
   password: "fakePassword",
+  currency: currencies[0],
 };
 
 export const updatedUser: UserI = {
@@ -15,6 +18,7 @@ export const updatedUser: UserI = {
   name: "fakeUpdatedName",
   email: "fakeUpdatedEmail",
   password: "fakeUpdatedPassword",
+  currency: currencies[0],
 };
 
 export const loggedUser: LoginI = {
@@ -22,40 +26,39 @@ export const loggedUser: LoginI = {
   password: "fakePassword",
 };
 
-
 export const newTransaction: TransactionFormI = {
-    _id: "01",
-    type: "income",
-    concept: "August Salary",
-    category: { _id: "66da1b9328ba43a7f62749d2", label: "Salary" },
-    amount: "999",
-    userId: "1234",
-  };
-  
-  export const updatedTransaction: TransactionFormI = {
-    _id: "01",
-    type: "income",
-    concept: "September Salary",
-    category: { _id: "66da1b9328ba43a7f62749d2", label: "Salary" },
-    amount: "888",
-    userId: "1234",
-  };
-  
-  export const categories = [{ _id: "01", label: "Salary" }];
+  _id: "01",
+  type: "income",
+  concept: "August Salary",
+  category: { _id: "66da1b9328ba43a7f62749d2", label: "Salary" },
+  amount: "999",
+  userId: "1234",
+};
 
-  export const createWrapper = (withUser: boolean) => {
-    const wrapper = ({ children }: { children: React.ReactNode }) => (
-      <RecoilRoot
-        initializeState={
-          withUser
-            ? ({ set }) => {
-                set(userState, newUser);
-              }
-            : undefined
-        }
-      >
-        {children}
-      </RecoilRoot>
-    );
-    return wrapper;
-  };
+export const updatedTransaction: TransactionFormI = {
+  _id: "01",
+  type: "income",
+  concept: "September Salary",
+  category: { _id: "66da1b9328ba43a7f62749d2", label: "Salary" },
+  amount: "888",
+  userId: "1234",
+};
+
+export const categories = [{ _id: "01", label: "Salary" }];
+
+export const createWrapper = (withUser: boolean) => {
+  const wrapper = ({ children }: { children: React.ReactNode }) => (
+    <RecoilRoot
+      initializeState={
+        withUser
+          ? ({ set }) => {
+              set(userState, newUser);
+            }
+          : undefined
+      }
+    >
+      {children}
+    </RecoilRoot>
+  );
+  return wrapper;
+};
