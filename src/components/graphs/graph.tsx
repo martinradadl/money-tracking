41d97471d2d27acddc86b708506ecabda6489566
Martin Rada <martinradadl@gmail.com>
2024-12-05 15:00:34 -0500

    Graph component, page and skeleton created

---
+++ src/components/graphs/graph.tsx
@@ -0,0 +1,7 @@
+export const Graph = () => {
+  return (
+    <div className="bg-gray w-full aspect-square rounded">
+      <h1 className="text-beige font-bold text-lg">This is a Graph</h1>
+    </div>
+  );
+};
