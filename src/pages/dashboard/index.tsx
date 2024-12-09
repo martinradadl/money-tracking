9ba4ecdc7f6d49e974cd95ea325b4a2b4afb4be8
Martin Rada <martinradadl@gmail.com>
2024-12-09 17:28:41 -0500

    Donut charts added for balances

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -8,12 +8,28 @@ import { GraphSkeleton } from "../../components/graphs/graph-skeleton";
 import { Graph } from "../../components/graphs/graph";
 import { useNavigate } from "react-router-dom";
 import { useDashboard } from "../../data/dashboard";
+import { DonutChart } from "@carbon/charts-react";
+import "@carbon/charts-react/styles.css";
 
 export const Dashboard: React.FC = () => {
   const navigate = useNavigate();
   const [user] = useRecoilState(userState);
-  const { balance, getBalance } = useTransactions();
-  const { balance: debtsBalance, getBalance: getDebtsBalance } = useDebts();
+  const {
+    balance,
+    getBalance,
+    totalIncome,
+    getTotalIncome,
+    totalExpenses,
+    getTotalExpenses,
+  } = useTransactions();
+  const {
+    balance: debtsBalance,
+    getBalance: getDebtsBalance,
+    totalLoans,
+    getTotalLoans,
+    totalDebts,
+    getTotalDebts,
+  } = useDebts();
   const { totalBalance, getTotalBalance } = useDashboard();
   const [isLoading, setIsLoading] = useState(true);
 
@@ -22,6 +38,10 @@ export const Dashboard: React.FC = () => {
       await getBalance();
       await getDebtsBalance();
       await getTotalBalance();
+      await getTotalIncome();
+      await getTotalExpenses();
+      await getTotalLoans();
+      await getTotalDebts();
       setIsLoading(false);
     }
   };
@@ -29,6 +49,104 @@ export const Dashboard: React.FC = () => {
   useEffect(() => {
     getBalances();
   }, [user?._id]);
+
+  const transactionsColors = {
+    Income: "#41B06E",
+    Expenses: "rgb(220,38,38)",
+  };
+
+  const debtsColors = {
+    Loans: "#41B06E",
+    Debts: "rgb(220,38,38)",
+  };
+
+  const totalBalanceColors = {
+    Income: "#41B06E",
+    Expenses: "rgb(220,38,38)",
+    Loans: "blue",
+    Debts: "orange",
+  };
+
+  const transactionsData = [
+    {
+      group: "Income",
+      value: totalIncome,
+    },
+    {
+      group: "Expenses",
+      value: totalExpenses,
+    },
+  ];
+
+  const debtsData = [
+    {
+      group: "Loans",
+      value: totalLoans,
+    },
+    {
+      group: "Debts",
+      value: totalDebts,
+    },
+  ];
+
+  const totalBalanceData = [...transactionsData, ...debtsData];
+
+  const transactionsOptions = {
+    title: "Transactions",
+    resizable: true,
+    color: {
+      scale: transactionsColors,
+    },
+    legend: {
+      alignment: "center",
+    },
+    donut: {
+      alignment: "center",
+      center: {
+        number: balance,
+        numberFormatter: (balance: number) =>
+          balance >= 0 ? `$${balance}` : `-$${balance * -1}`,
+      },
+    },
+  };
+
+  const debtsOptions = {
+    title: "Debts",
+    resizable: true,
+    color: {
+      scale: debtsColors,
+    },
+    legend: {
+      alignment: "center",
+    },
+    donut: {
+      alignment: "center",
+      center: {
+        number: debtsBalance,
+        numberFormatter: (debtsBalance: number) =>
+          debtsBalance >= 0 ? `$${debtsBalance}` : `-$${debtsBalance * -1}`,
+      },
+    },
+  };
+
+  const totalBalanceOptions = {
+    title: "Total Balance",
+    resizable: true,
+    color: {
+      scale: totalBalanceColors,
+    },
+    legend: {
+      alignment: "center",
+    },
+    donut: {
+      alignment: "center",
+      center: {
+        number: totalBalance,
+        numberFormatter: (totalBalance: number) =>
+          totalBalance >= 0 ? `$${totalBalance}` : `-$${totalBalance * -1}`,
+      },
+    },
+  };
 
   return (
     <div className="flex flex-col flex-1 pt-2 pb-14 px-5 gap-4 overflow-y-auto entrance-anim">
@@ -66,6 +184,17 @@ export const Dashboard: React.FC = () => {
                 })
               : null}
           </p>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center bg-beige rounded p-2">
+        <DonutChart data={totalBalanceData} options={totalBalanceOptions} />
+      </div>
+      <div className="flex flex-wrap bg-beige rounded p-2">
+        <div className="flex flex-1">
+          <DonutChart data={transactionsData} options={transactionsOptions} />
+        </div>
+        <div className="flex flex-1">
+          <DonutChart data={debtsData} options={debtsOptions} />
         </div>
       </div>
       <h1 className="text-beige text-2xl font-semibold">Your Stats: </h1>
