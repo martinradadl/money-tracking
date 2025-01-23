50937509175a19ec94361b6f6130ca815d1f3dbb
Martin Rada <martinradadl@gmail.com>
2025-01-23 18:03:55 -0500

    old graph container and skeleton deleted

--- src/components/graphs/graph.tsx
+++
@@ -1,7 +0,0 @@
-export const Graph = () => {
-  return (
-    <div className="bg-gray w-full aspect-square rounded">
-      <h1 className="text-beige font-bold text-lg">This is a Graph</h1>
-    </div>
-  );
-};
