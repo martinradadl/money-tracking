fe953c4e8352f804d4296fa8512f8b642ba62662
Martin Rada <104103475+martinradadl@users.noreply.github.com>
2025-02-21 18:35:17 -0500

    Improve/charts and splash screen responsiveness (#38)
    * Progress bas added
    
    * Container added for charts without data
    
    * Progress bar styles improved

--- src/components/splash-screen.tsx
+++ src/components/splash-screen.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {
   getCurrencies,
   logout,
@@ -10,17 +10,32 @@ import { user } from "../helpers/cookies";
 import { isExpired } from "../helpers/utils";
 
 export const SplashScreen = () => {
+  const [progress, setProgress] = useState(0);
+
   useEffect(() => {
     Promise.all([getCurrencies(), getCategories()]).then(() => {
+      const min = 0.33;
+      const max = 0.66;
+      setProgress(Math.random() * (max - min) + min);
+
       const userCookieValue = user();
       if (userCookieValue) {
         setUser(userCookieValue);
       }
+
       setTimeout(() => {
-        setIsSplashScreenLoading(false);
+        setProgress(1);
       }, 1000);
     });
   }, []);
+
+  useEffect(() => {
+    if (progress === 1) {
+      setTimeout(() => {
+        setIsSplashScreenLoading(false);
+      }, 500);
+    }
+  }, [progress]);
 
   useEffect(() => {
     if (isExpired()) {
@@ -30,8 +45,18 @@ export const SplashScreen = () => {
   }, []);
 
   return (
-    <div className="bg-green text-navy">
-      <h1>Loading</h1>
+    <div className="bg-green text-navy h-dvh flex flex-col items-center justify-center gap-4">
+      <h1 className="text-3xl font-semibold">Money Tracking</h1>
+      <div className="flex flex-col items-center gap-2">
+        <h2 className="text-2xl">Loading...</h2>
+
+        <div className="bg-light-gray rounded w-40 h-4">
+          <div
+            className="bg-dark-green rounded h-4"
+            style={{ width: `${progress * 100}%` }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
