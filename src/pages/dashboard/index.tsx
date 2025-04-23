b6ebcbc486f0a237b6e60ec6017e31f00a436763
Martin Rada <martinradadl@gmail.com>
2025-04-23 17:19:12 -0500

    Scroll bar position and responsive design improved

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -40,124 +40,126 @@ export const Dashboard: React.FC = () => {
   }, [user?._id]);
 
   return (
-    <div className="flex flex-col m-auto flex-1 pt-2 pb-14 px-5 gap-4 overflow-y-auto entrance-anim w-4/5 max-w-[1000px]">
-      <h1 className="page-title text-beige">Hi, {user?.name}</h1>
-      <h2 className="text-beige text-2xl font-semibold">Your Balances: </h2>
-      <div className="bg-beige rounded p-2 flex flex-wrap gap-1">
-        <div className="w-full md:flex-1 pb-2">
-          <h3 className="text-navy text-xl font-semibold text-center">
-            Total Balance{" "}
-          </h3>
-          {TOTAL_BALANCE.data[0].value === 0 &&
-          TOTAL_BALANCE.data[1].value === 0 ? (
-            <div className="flex flex-col justify-center">
-              <NoDataChart />
-            </div>
-          ) : (
-            <div className="w-full md:flex-1 pb-2">
-              <div className="flex w-full aspect-square p-2 md:flex-1 md:w-auto">
-                <DonutChart
-                  data={TOTAL_BALANCE.data}
-                  options={TOTAL_BALANCE.options}
-                />
+    <div className="flex flex-col w-full items-center overflow-y-auto">
+      <div className="flex flex-col flex-1 pt-2 pb-14 px-5 gap-4 entrance-anim md:w-4/5 max-w-[1000px]">
+        <h1 className="page-title text-beige">Hi, {user?.name}</h1>
+        <h2 className="text-beige text-2xl font-semibold">Your Balances: </h2>
+        <div className="bg-beige rounded p-2 flex flex-wrap gap-1">
+          <div className="w-full md:flex-1 pb-2">
+            <h3 className="text-navy text-xl font-semibold text-center">
+              Total Balance{" "}
+            </h3>
+            {TOTAL_BALANCE.data[0].value === 0 &&
+            TOTAL_BALANCE.data[1].value === 0 ? (
+              <div className="flex flex-col justify-center">
+                <NoDataChart />
               </div>
-              <button
-                onClick={() => {
-                  navigate(`/graph?graphCode=TOTAL_BALANCE`);
-                }}
-                className="bg-navy text-beige font-bold block m-auto rounded py-1 px-2 self-center text-sm focus:outline-none data-[focus]:outline-1 data-[focus]:outline-white"
-              >
-                Full Screen
-              </button>
-            </div>
-          )}
-        </div>
-        <div className="w-full md:flex-1 pb-2">
-          <h3 className="text-navy text-xl font-semibold text-center">
-            Detailed Balance
-          </h3>
-          {TOTAL_BALANCE_DETAILED.data[0].value === 0 &&
-          TOTAL_BALANCE_DETAILED.data[1].value === 0 ? (
-            <div className="flex flex-col justify-center">
-              <NoDataChart />
-            </div>
-          ) : (
-            <div className="w-full md:flex-1 pb-2">
-              <div className="flex w-full aspect-square p-2 md:flex-1 md:w-auto">
-                <DonutChart
-                  data={TOTAL_BALANCE_DETAILED.data}
-                  options={TOTAL_BALANCE_DETAILED.options}
-                />
+            ) : (
+              <div className="w-full md:flex-1 pb-2">
+                <div className="flex w-full aspect-square p-2 md:flex-1 md:w-auto">
+                  <DonutChart
+                    data={TOTAL_BALANCE.data}
+                    options={TOTAL_BALANCE.options}
+                  />
+                </div>
+                <button
+                  onClick={() => {
+                    navigate(`/graph?graphCode=TOTAL_BALANCE`);
+                  }}
+                  className="bg-navy text-beige font-bold block m-auto rounded py-1 px-2 self-center text-sm focus:outline-none data-[focus]:outline-1 data-[focus]:outline-white"
+                >
+                  Full Screen
+                </button>
               </div>
-              <button
-                onClick={() => {
-                  navigate(`/graph?graphCode=TOTAL_BALANCE_DETAILED`);
-                }}
-                className="bg-navy text-beige font-bold block m-auto rounded py-1 px-2 self-center text-sm focus:outline-none data-[focus]:outline-1 data-[focus]:outline-white"
-              >
-                Full Screen
-              </button>
-            </div>
-          )}
+            )}
+          </div>
+          <div className="w-full md:flex-1 pb-2">
+            <h3 className="text-navy text-xl font-semibold text-center">
+              Detailed Balance
+            </h3>
+            {TOTAL_BALANCE_DETAILED.data[0].value === 0 &&
+            TOTAL_BALANCE_DETAILED.data[1].value === 0 ? (
+              <div className="flex flex-col justify-center">
+                <NoDataChart />
+              </div>
+            ) : (
+              <div className="w-full md:flex-1 pb-2">
+                <div className="flex w-full aspect-square p-2 md:flex-1 md:w-auto">
+                  <DonutChart
+                    data={TOTAL_BALANCE_DETAILED.data}
+                    options={TOTAL_BALANCE_DETAILED.options}
+                  />
+                </div>
+                <button
+                  onClick={() => {
+                    navigate(`/graph?graphCode=TOTAL_BALANCE_DETAILED`);
+                  }}
+                  className="bg-navy text-beige font-bold block m-auto rounded py-1 px-2 self-center text-sm focus:outline-none data-[focus]:outline-1 data-[focus]:outline-white"
+                >
+                  Full Screen
+                </button>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-      <div className="bg-beige rounded p-2 flex flex-wrap gap-1">
-        <div className="w-full md:flex-1 pb-2">
-          <h3 className="text-navy text-xl font-semibold text-center">
-            Transactions{" "}
-          </h3>
+        <div className="bg-beige rounded p-2 flex flex-wrap gap-1">
+          <div className="w-full md:flex-1 pb-2">
+            <h3 className="text-navy text-xl font-semibold text-center">
+              Transactions{" "}
+            </h3>
 
-          {TRANSACTIONS_BALANCE.data[0].value === 0 &&
-          TRANSACTIONS_BALANCE.data[1].value === 0 ? (
-            <div className="flex flex-col justify-center">
-              <NoDataChart />
-            </div>
-          ) : (
-            <>
-              <div className="flex w-full aspect-square p-2 md:flex-1 md:w-auto">
-                <DonutChart
-                  data={TRANSACTIONS_BALANCE.data}
-                  options={TRANSACTIONS_BALANCE.options}
-                />
+            {TRANSACTIONS_BALANCE.data[0].value === 0 &&
+            TRANSACTIONS_BALANCE.data[1].value === 0 ? (
+              <div className="flex flex-col justify-center">
+                <NoDataChart />
               </div>
-              <button
-                onClick={() => {
-                  navigate(`/graph?graphCode=TRANSACTIONS_BALANCE`);
-                }}
-                className="bg-navy text-beige font-bold block m-auto rounded py-1 px-2 self-center text-sm focus:outline-none data-[focus]:outline-1 data-[focus]:outline-white"
-              >
-                Full Screen
-              </button>
-            </>
-          )}
-        </div>
-        <div className="w-full md:flex-1 pb-2">
-          <h3 className="text-navy text-xl font-semibold text-center">
-            Debts{" "}
-          </h3>
-          {DEBTS_BALANCE.data[0].value === 0 &&
-          DEBTS_BALANCE.data[1].value === 0 ? (
-            <div className="flex flex-col justify-center">
-              <NoDataChart />
-            </div>
-          ) : (
-            <>
-              <div className="flex w-full aspect-square p-2 md:flex-1 md:w-auto">
-                <DonutChart
-                  data={DEBTS_BALANCE.data}
-                  options={DEBTS_BALANCE.options}
-                />
+            ) : (
+              <>
+                <div className="flex w-full aspect-square p-2 md:flex-1 md:w-auto">
+                  <DonutChart
+                    data={TRANSACTIONS_BALANCE.data}
+                    options={TRANSACTIONS_BALANCE.options}
+                  />
+                </div>
+                <button
+                  onClick={() => {
+                    navigate(`/graph?graphCode=TRANSACTIONS_BALANCE`);
+                  }}
+                  className="bg-navy text-beige font-bold block m-auto rounded py-1 px-2 self-center text-sm focus:outline-none data-[focus]:outline-1 data-[focus]:outline-white"
+                >
+                  Full Screen
+                </button>
+              </>
+            )}
+          </div>
+          <div className="w-full md:flex-1 pb-2">
+            <h3 className="text-navy text-xl font-semibold text-center">
+              Debts{" "}
+            </h3>
+            {DEBTS_BALANCE.data[0].value === 0 &&
+            DEBTS_BALANCE.data[1].value === 0 ? (
+              <div className="flex flex-col justify-center">
+                <NoDataChart />
               </div>
-              <button
-                onClick={() => {
-                  navigate(`/graph?graphCode=DEBTS_BALANCE`);
-                }}
-                className="bg-navy text-beige font-bold block m-auto rounded py-1 px-2 text-sm focus:outline-none data-[focus]:outline-1 data-[focus]:outline-white"
-              >
-                Full Screen
-              </button>
-            </>
-          )}
+            ) : (
+              <>
+                <div className="flex w-full aspect-square p-2 md:flex-1 md:w-auto">
+                  <DonutChart
+                    data={DEBTS_BALANCE.data}
+                    options={DEBTS_BALANCE.options}
+                  />
+                </div>
+                <button
+                  onClick={() => {
+                    navigate(`/graph?graphCode=DEBTS_BALANCE`);
+                  }}
+                  className="bg-navy text-beige font-bold block m-auto rounded py-1 px-2 text-sm focus:outline-none data-[focus]:outline-1 data-[focus]:outline-white"
+                >
+                  Full Screen
+                </button>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </div>
