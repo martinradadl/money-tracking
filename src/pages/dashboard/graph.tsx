12d229d3cbf10b9e6c2165678cf49334e5581fa6
Martin Rada <martinradadl@gmail.com>
2025-04-24 21:26:00 -0500

    Graph filtering updated

--- src/pages/dashboard/graph.tsx
+++ src/pages/dashboard/graph.tsx
@@ -10,28 +10,27 @@ import { useAuth } from "../../data/authentication";
 import { useShallow } from "zustand/shallow";
 import DatePicker from "react-datepicker";
 import {
+  filterFormInitialState,
+  FilterMovementForm,
   filterTypes,
   formatDateByPeriod,
   GetAmountsSumParams,
-  TimePeriod,
   timePeriods,
 } from "../../helpers/movements";
 import { removeCookie } from "../../helpers/cookies";
 import { NoDataChart } from "../../components/no-data-chart";
+import { useCategories } from "../../data/categories";
+import { MdCancel } from "react-icons/md";
+import { noCategory } from "../../helpers/categories";
 
 export const GraphPage: React.FC = () => {
   const [searchParams] = useSearchParams();
   const graphCode = searchParams.get("graphCode") || "";
-  const [selectedFilterType, setSelectedFilterType] = useState(
-    filterTypes.singleDate
+  const [selectedFilters, setSelectedFilters] = useState<FilterMovementForm>(
+    filterFormInitialState
   );
-  const [selectedTimePeriod, setSelectedTimePeriod] = useState(timePeriods.day);
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-  const [selectedDateRange, setSelectedDateRange] = useState<(Date | null)[]>([
-    null,
-    null,
-  ]);
-  const [startDate, endDate] = selectedDateRange;
+  const { timePeriod, date, dateRange, category, type } = selectedFilters;
+  const [startDate, endDate] = dateRange;
   const navigate = useNavigate();
   const { mappedDataAndOptions } = useGraphs();
   const { data, options } = mappedDataAndOptions[graphCode];
@@ -40,6 +39,12 @@ export const GraphPage: React.FC = () => {
       user: state.user,
     }))
   );
+  const { categories } = useCategories(
+    useShallow((state) => ({
+      categories: state.categories,
+    }))
+  );
+
   const getBalances = async (params: GetAmountsSumParams) => {
     Promise.all([
       getTotalIncome(params),
@@ -48,30 +53,23 @@ export const GraphPage: React.FC = () => {
       getTotalDebts(params),
     ]);
   };
+
   const getAmountsSumParams = () => {
-    const selectedTimePeriodKey = selectedTimePeriod.toLowerCase();
-    const params = {
-      timePeriod: selectedTimePeriodKey,
-      selectedDate: "",
-      selectedStartDate: "",
-      selectedEndDate: "",
-    };
-    if (selectedDate)
-      params.selectedDate = formatDateByPeriod(
-        selectedTimePeriodKey,
-        selectedDate
-      );
-    else if (selectedDateRange[0] && selectedDateRange[1]) {
-      params.selectedStartDate = formatDateByPeriod(
-        selectedTimePeriodKey,
-        selectedDateRange[0]
-      );
-      params.selectedStartDate = formatDateByPeriod(
-        selectedTimePeriodKey,
-        selectedDateRange[1]
-      );
-    } else {
+    if (!date && !dateRange[0] && !dateRange[1] && category === noCategory) {
       return {};
+    }
+    const timePeriodKey = timePeriod.toLowerCase();
+    const params = {
+      timePeriod: timePeriodKey,
+      date: "",
+      startDate: "",
+      endDate: "",
+      category: category._id,
+    };
+    if (date) params.date = formatDateByPeriod(timePeriodKey, date);
+    else if (dateRange[0] && dateRange[1]) {
+      params.startDate = formatDateByPeriod(timePeriodKey, dateRange[0]);
+      params.endDate = formatDateByPeriod(timePeriodKey, dateRange[1]);
     }
     return params;
   };
@@ -105,32 +103,41 @@ export const GraphPage: React.FC = () => {
         removeCookie("incomeCache");
         removeCookie("expensesCache");
       }
-      getBalances(getAmountsSumParams());
+      const params = getAmountsSumParams();
+      getBalances(params);
     }
-  }, [selectedDate, selectedDateRange]);
+  }, [date, dateRange, category]);
 
   const handleChangeFilterType = (
     event: React.ChangeEvent<HTMLSelectElement>
   ) => {
-    setSelectedFilterType(event?.target.value);
     if (event?.target.value === filterTypes.singleDate) {
-      setSelectedDateRange([null, null]);
+      setSelectedFilters({
+        ...selectedFilters,
+        dateRange: filterFormInitialState.dateRange,
+      });
     } else {
-      setSelectedDate(null);
+      setSelectedFilters({
+        ...selectedFilters,
+        date: filterFormInitialState.date,
+      });
     }
+    setSelectedFilters({ ...selectedFilters, type: event?.target.value });
   };
 
   const handleChangeTimePeriod = (
     event: React.ChangeEvent<HTMLSelectElement>
   ) => {
-    setSelectedTimePeriod(event?.target.value as TimePeriod);
+    setSelectedFilters({ ...selectedFilters, timePeriod: event?.target.value });
   };
 
-  const handleCleanFilter = () => {
-    setSelectedDateRange([null, null]);
-    setSelectedDate(null);
-    setSelectedFilterType(filterTypes.singleDate);
-    setSelectedTimePeriod(timePeriods.day);
+  const handleChangeCategory = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSelectedFilters({
+      ...selectedFilters,
+      category: { _id: event.target.value, label: "" },
+    });
   };
 
   return (
@@ -144,11 +151,11 @@ export const GraphPage: React.FC = () => {
         />
       </div>
 
-      <h1 className="text-2xl text-beige">Filter by:</h1>
+      <h1 className="text-2xl text-beige">Filter by Date:</h1>
       <Select
         name="filter-type"
         id="filter-type"
-        value={selectedFilterType}
+        value={type}
         onChange={handleChangeFilterType}
         className="w-full text-xl rounded bg-navy text-beige border-b-2"
       >
@@ -163,7 +170,7 @@ export const GraphPage: React.FC = () => {
       <Select
         name="time-period"
         id="time-period"
-        value={selectedTimePeriod}
+        value={timePeriod}
         onChange={handleChangeTimePeriod}
         className="w-full text-xl rounded bg-navy text-beige border-b-2"
       >
@@ -175,22 +182,25 @@ export const GraphPage: React.FC = () => {
           );
         })}
       </Select>
-      {selectedFilterType === filterTypes.singleDate ? (
+      {type === filterTypes.singleDate ? (
         <DatePicker
           className="w-full h-9 px-2 rounded border-beige bg-navy text-beige border-b-2"
           placeholderText="Select a Date"
-          selected={selectedDate}
-          onChange={(date) => setSelectedDate(date)}
+          selected={date}
+          onChange={(date) => {
+            setSelectedFilters({ ...selectedFilters, date });
+          }}
           dateFormat={
-            selectedTimePeriod === timePeriods.day
+            timePeriod === timePeriods.day
               ? undefined
-              : selectedTimePeriod === timePeriods.month
+              : timePeriod === timePeriods.month
               ? "MM/yyyy"
               : "yyyy"
           }
-          showMonthYearPicker={selectedTimePeriod === timePeriods.month}
-          showYearPicker={selectedTimePeriod === timePeriods.year}
-          showYearDropdown={selectedTimePeriod === timePeriods.day}
+          showMonthYearPicker={timePeriod === timePeriods.month}
+          showYearPicker={timePeriod === timePeriods.year}
+          showYearDropdown={timePeriod === timePeriods.day}
+          isClearable
         />
       ) : (
         <DatePicker
@@ -199,25 +209,58 @@ export const GraphPage: React.FC = () => {
           selectsRange
           startDate={startDate}
           endDate={endDate}
-          onChange={(update) => {
-            setSelectedDateRange(update);
-          }}
+          onChange={(dateRange) =>
+            setSelectedFilters({ ...selectedFilters, dateRange })
+          }
           dateFormat={
-            selectedTimePeriod === timePeriods.day
+            timePeriod === timePeriods.day
               ? undefined
-              : selectedTimePeriod === timePeriods.month
+              : timePeriod === timePeriods.month
               ? "MM/yyyy"
               : "yyyy"
           }
-          isClearable={selectedTimePeriod === timePeriods.day}
-          showMonthYearPicker={selectedTimePeriod === timePeriods.month}
-          showYearPicker={selectedTimePeriod === timePeriods.year}
-          showYearDropdown={selectedTimePeriod === timePeriods.day}
+          isClearable
+          showMonthYearPicker={timePeriod === timePeriods.month}
+          showYearPicker={timePeriod === timePeriods.year}
+          showYearDropdown={timePeriod === timePeriods.day}
         />
       )}
+      <h1 className="text-2xl text-beige">Filter by Category:</h1>
+      <div className="flex items-center gap-1">
+        <Select
+          name="category"
+          id="category"
+          value={category._id}
+          onChange={handleChangeCategory}
+          className="w-full text-xl rounded bg-navy text-beige border-b-2"
+        >
+          <option style={{ display: "none" }}></option>
+          {categories.map((elem, i) => {
+            return (
+              <option key={i} value={elem._id}>
+                {elem.label}
+              </option>
+            );
+          })}
+        </Select>
+        {selectedFilters.category._id !== "" ? (
+          <MdCancel
+            className="text-2xl text-beige"
+            onClick={() => {
+              setSelectedFilters({
+                ...selectedFilters,
+                category: noCategory,
+              });
+            }}
+          />
+        ) : null}
+      </div>
+
       <Button
         className="w-full rounded-md bg-yellow-category text-navy py-1 px-3 text-xl font-semibold"
-        onClick={handleCleanFilter}
+        onClick={() => {
+          setSelectedFilters(filterFormInitialState);
+        }}
       >
         Clear Filter
       </Button>
